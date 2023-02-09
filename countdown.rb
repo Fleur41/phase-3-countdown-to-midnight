6d5b4a7214faad452a8efecc079fdc7a0d7c45d8
Fleur41 <Muriithikings456@gmail.com>
2023-02-09 17:07:29 +0300

    Completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+
+def countdown number
+    while number > 0  do
+        puts "#{number} SECOND(S)!"
+        number-=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep number
+while number > 0  do
+    puts "#{number} SECOND(S)!"
+    sleep 5 #Pauses execution for 5 second
+    number-=1
+end
+"HAPPY NEW YEAR!"
 end
